6e2a79242a1e035524d325971f0aafeaf78d0507
Elise Hoovler <elisejhoovler@live.com>
2021-01-04 04:20:40 -0600

    editing

--- web/pages/index.js
+++ web/pages/index.js
@@ -12,7 +12,6 @@ class IndexPage extends React.Component {
     return (
       <Layout config={config}>
         <h1>No route set</h1>
-        <h2>Setup automatic routes in sanity or custom routes in next.config.js</h2>
       </Layout>
     )
   }
